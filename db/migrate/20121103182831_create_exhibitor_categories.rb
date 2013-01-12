146f539123d2886672e879712c74c142cfa11aba
unknown <sdbmich@yahoo.com>
2013-01-12 15:54:57 -0800

    update exhibitor migration file

--- db/migrate/20121103182831_create_exhibitor_categories.rb
+++ db/migrate/20121103182831_create_exhibitor_categories.rb
@@ -1,5 +1,15 @@
 class CreateExhibitorCategories < ActiveRecord::Migration
   def self.up
+    create_table :exhibitor_categories do |t|
+      t.integer :event_id
+      t.integer :show_category_id
+      t.integer :exhibitor_id
+      t.string :hide
+      t.string :status
+      t.integer :sortkey
+
+      t.timestamps
+    end
     add_index :exhibitor_categories, [:event_id, :exhibitor_id, :show_category_id], :name => "event_show_exhibitor_idx", :unique => true
     add_index :exhibitor_categories, :show_category_id
   end
