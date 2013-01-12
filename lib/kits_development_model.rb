3f76bca56fba9d46ff9e0223129a197e71a35c3c
unknown <sdbmich@yahoo.com>
2013-01-12 13:31:29 -0800

    update kits-development-model rb file

--- lib/kits_development_model.rb
+++ lib/kits_development_model.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-class KitsModel < ActiveRecord::Base
+class KitsDevelopmentModel < ActiveRecord::Base
   self.abstract_class = true
   establish_connection "kits_development"
 end
